b318bad0a5af7f5aec274f776921f3483c7386cf
Ivor <ivorgradinski@gmail.com>
2022-07-13 03:02:50 +0200

    final things

--- app.py
+++ app.py
@@ -33,30 +33,43 @@ with dataset:
 with modelTraining:
     st.header('Time to train the model')
     st.text('Choose parameterers')
-    duration = modelTraining.slider('Trajanje u ms', min_value=1,
-                                    max_value=1800000, value=1)
+
+    duration = st.number_input(
+        'Trajanje u ms', min_value=0.0, max_value=1800000.0, step=100.0)
+
     acousticness = st.number_input(
         'Akusticnost', min_value=0.0, max_value=1.0, step=1e-5, format="%.5f")
+
     danceability = modelTraining.slider(
         'Plesnost', min_value=10, max_value=20, value=15)
+
     energy = modelTraining.slider(
         'Energicnost', min_value=10, max_value=20, value=15)
-    instrumentalness = modelTraining.slider('Instrumentalnost', min_value=10,
-                                            max_value=20, value=15)
+
+    instrumentalness = modelTraining.slider(
+        'Instrumentalnost', min_value=10, max_value=20, value=15)
+
     key = modelTraining.slider('Kljuc', min_value=10, max_value=20, value=15)
+
     liveness = modelTraining.slider(
         'Zivost', min_value=10, max_value=20, value=15)
+
     loudness = modelTraining.slider(
         'Glasnoca', min_value=10, max_value=20, value=15)
+
     audio_mode = modelTraining.slider(
         'Audio mode', min_value=10, max_value=20, value=15)
+
     speechiness = modelTraining.slider(
         'Govornost', min_value=10, max_value=20, value=15)
+
     tempo = modelTraining.slider('Tempo', min_value=10, max_value=20, value=15)
-    time_signature = modelTraining.slider('Vremenski potpis', min_value=10,
-                                          max_value=20, value=15)
-    audio_valence = modelTraining.slider('Audio valencija', min_value=10,
-                                         max_value=20, value=15)
+
+    time_signature = modelTraining.slider(
+        'Vremenski potpis', min_value=10, max_value=20, value=15)
+
+    audio_valence = modelTraining.slider(
+        'Audio valencija', min_value=10, max_value=20, value=15)
 
     # num = st.number_input(
     #     "Higher precision step",
