1da8cb0aab834317cf61fa0e3f95024ca2c34e86
Ivor <ivorgradinski@gmail.com>
2022-07-13 02:13:24 +0200

    final things

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ with header:
 
 with dataset:
     song_data = get_data('data/song_data.csv')
-    st.write(song_data.head())
+    st.write(song_data.describe())
 
 with modelTraining:
     st.header('Time to train the model')
@@ -44,15 +44,19 @@ with modelTraining:
     instrumentalness = modelTraining.slider('Instrumentalnost', min_value=10,
                                             max_value=20, value=15)
     key = modelTraining.slider('Kljuc', min_value=10, max_value=20, value=15)
-    modelTraining.slider('Zivost', min_value=10, max_value=20, value=15)
-    modelTraining.slider('Glasnoca', min_value=10, max_value=20, value=15)
-    modelTraining.slider('Audio mode', min_value=10, max_value=20, value=15)
-    modelTraining.slider('Govornost', min_value=10, max_value=20, value=15)
-    modelTraining.slider('Tempo', min_value=10, max_value=20, value=15)
-    modelTraining.slider('Vremenski potpis', min_value=10,
-                         max_value=20, value=15)
-    modelTraining.slider('Audio valencija', min_value=10,
-                         max_value=20, value=15)
+    liveness = modelTraining.slider(
+        'Zivost', min_value=10, max_value=20, value=15)
+    loudness = modelTraining.slider(
+        'Glasnoca', min_value=10, max_value=20, value=15)
+    audio_mode = modelTraining.slider(
+        'Audio mode', min_value=10, max_value=20, value=15)
+    speechiness = modelTraining.slider(
+        'Govornost', min_value=10, max_value=20, value=15)
+    tempo = modelTraining.slider('Tempo', min_value=10, max_value=20, value=15)
+    time_signature = modelTraining.slider('Vremenski potpis', min_value=10,
+                                          max_value=20, value=15)
+    audio_valence = modelTraining.slider('Audio valencija', min_value=10,
+                                         max_value=20, value=15)
 
     num = st.number_input(
         "Higher precision step",
@@ -60,6 +64,3 @@ with modelTraining:
         max_value=5.0,
         step=1e-6,
         format="%.5f")
-
-st.write(num)
-st.write(key)
